9c0874b6eee4612beec84edf14d824a64b5e4c9f
ahzvenol <ahzvenol@users.noreply.github.com>
2024-10-23 23:36:14 +0800

    feat: 自定义Tab增强,支持移除全部Tab(伪),使用*启用

--- app/src/main/java/com/shatyuka/zhiliao/hooks/TopTabs.kt
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/TopTabs.kt
@@ -2,10 +2,10 @@ package com.shatyuka.zhiliao.hooks
 
 import com.shatyuka.zhiliao.Helper
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedBridge.hookMethod
 import de.robv.android.xposed.XposedHelpers.getObjectField
 import de.robv.android.xposed.XposedHelpers.setObjectField
-import java.util.stream.Collectors
 
 /**
  * api: /root/tab
@@ -16,6 +16,9 @@ class TopTabs : BaseHook() {
 
     private var activityInfo: Class<*>? = null
     private var topTabs: Class<*>? = null
+    private var tabInfo: Class<*>? = null
+    private var tabStyle: Class<*>? = null
+    private var textTab: Class<*>? = null
 
     override fun getName(): String {
         return "自定义首页顶部Tab(TopTabs)"
@@ -33,18 +36,32 @@ class TopTabs : BaseHook() {
         } catch (e: Exception) {
             logE(e.message)
         }
+
+        try {
+            tabInfo = classLoader.loadClass("com.zhihu.android.api.model.TopTabInfo")
+        } catch (e: Exception) {
+            logE(e.message)
+        }
+
+        try {
+            tabStyle = classLoader.loadClass("com.zhihu.android.api.model.TabStyle")
+        } catch (e: Exception) {
+            logE(e.message)
+        }
+
+        try {
+            textTab = classLoader.loadClass("com.zhihu.android.api.model.TextTab")
+        } catch (e: Exception) {
+            logE(e.message)
+        }
     }
 
     override fun hook() {
-        if (!Helper.prefs.getBoolean("switch_mainswitch", false)) {
-            return
-        }
+        Helper.prefs.getBoolean("switch_mainswitch", false).let { if (!it) return }
 
         hookMethod(Helper.JacksonHelper.ObjectReader_readValue, object : XC_MethodHook() {
             override fun afterHookedMethod(param: MethodHookParam) {
-                if (param.result == null) {
-                    return
-                }
+                if (param.result == null) return
                 when (param.result.javaClass) {
                     activityInfo -> {
                         setObjectField(param.result, "topActivity", null)
@@ -60,44 +77,47 @@ class TopTabs : BaseHook() {
 
     private fun postProcessTopTabs(topTabs: Any) {
         setObjectField(topTabs, "topActivity", null)
-        filterTabs(topTabs)
-    }
-
-    private fun filterTabs(topTabs: Any) {
-        val tabList = getObjectField(topTabs, "tabs") as List<*>
-
-        val resultTabList = ArrayList<Any>()
-        val tabTypeMap = HashMap<String, Any>()
-        val originTabTypeList = ArrayList<String>()
-
-        tabList.forEach {
-            val tabType = getObjectField(it, "tabType") as String
-            tabTypeMap[tabType] = it!!
-            originTabTypeList.add(tabType)
-        }
-
-        getAllowedTabTypeList().forEach {
-            val tab = tabTypeMap[it]
-            if (tab != null) {
-                resultTabList.add(tab)
+        val tabConfig = Helper.prefs.getString(PREFS_KEY_NAME, "")!!
+        if ("*" == tabConfig) {
+            val fakeTabInfo = makeFakeTabInfo()
+            XposedBridge.log(listOf(fakeTabInfo).toString())
+            setObjectField(topTabs, "tabs", listOf(fakeTabInfo))
+        } else {
+            val tabList = getObjectField(topTabs, "tabs") as List<*>
+            val originTabMap = tabList.associateBy { getObjectField(it, "tabType") as String }
+            val allowedTabTypeList = tabConfig.split("|").map { it.trim() }
+            val resultTabList = originTabMap.filterKeys { it in allowedTabTypeList }.values.toList()
+            if (resultTabList.isNotEmpty()) {
+                setObjectField(topTabs, "tabs", resultTabList)
+            } else {
+                resetAllowedTabTypeList(originTabMap.keys)
             }
         }
-
-        if (resultTabList.isNotEmpty()) {
-            setObjectField(topTabs, "tabs", resultTabList)
-        } else {
-            resetAllowedTabTypeList(originTabTypeList)
-        }
     }
 
-    private fun getAllowedTabTypeList(): List<String> {
-        return Helper.prefs.getString(PREFS_KEY_NAME, "")?.split("|")?.stream()
-            ?.map { it.trim() }?.collect(Collectors.toList()) ?: ArrayList()
+    private fun makeFakeTabInfo(): Any? {
+        val fakeTabInfo = tabInfo!!.constructors[0].newInstance()
+        val fakeTabStyle = tabStyle!!.constructors[0].newInstance()
+        val fakeTextTab = textTab!!.constructors[0].newInstance()
+        setObjectField(fakeTextTab, "title", "ㅤ")
+        setObjectField(fakeTextTab, "color", "#ffffff")
+        setObjectField(fakeTextTab, "colorNight", "#000000")
+        setObjectField(fakeTabStyle, "type", "text")
+        setObjectField(fakeTabStyle, "text", fakeTextTab)
+        setObjectField(fakeTabInfo, "tabType", "activity")
+        setObjectField(fakeTabInfo, "tabName", "")
+        setObjectField(fakeTabInfo, "clickableInBrowseMode", false)
+        setObjectField(fakeTabInfo, "moduleId", "")
+        setObjectField(fakeTabInfo, "url", "https://www.zhihu.com/parker/")
+        setObjectField(fakeTabInfo, "startTime", null)
+        setObjectField(fakeTabInfo, "endTime", null)
+        setObjectField(fakeTabInfo, "normal", fakeTabStyle)
+        setObjectField(fakeTabInfo, "selected", fakeTabStyle)
+        setObjectField(fakeTabInfo, "flipConfig", null)
+        return fakeTabInfo
     }
 
-    private fun resetAllowedTabTypeList(tabTypeList: List<String>) {
-        Helper.prefs.edit().putString(
-            PREFS_KEY_NAME, tabTypeList.stream().collect(Collectors.joining("|"))
-        ).apply()
+    private fun resetAllowedTabTypeList(tabTypeList: Collection<String>) {
+        Helper.prefs.edit().putString(PREFS_KEY_NAME, tabTypeList.joinToString { "|" }).apply()
     }
 }
